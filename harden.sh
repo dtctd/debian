c52532e2aea9954bc1686206f6035a4ce592389f
dtctd <udtctd@gmail.com>
2015-12-04 16:26:47 +0100

    .

--- harden.sh
+++ harden.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+function CheckEntry {
+    if grep -q $1 $2;
+    then
+        echo "entry '$1' allready present in '$2'"
+    else
+        echo $1 >> $2
+    fi
+
 # Configure iptables backup and restore
 echo "#!/bin/sh" > /etc/network/if-pre-up.d/iptables
 echo "/sbin/iptables-restore < /etc/iptables.up.rules" >> /etc/network/if-pre-up.d/iptables
@@ -9,21 +17,28 @@ chmod +x /etc/network/if-pre-up.d/iptables
 iptables -F INPUT
 iptables -F OUTPUT
 iptables -F FORWARD
-iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
-iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j DROP
-iptables -A INPUT -p tcp --dport 111 -j DROP
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -s 127.0.0.1/32 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
+-A INPUT -j DROP
+-A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A FORWARD -j DROP
+-A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A OUTPUT -m state --state NEW -j ACCEPT
 
 # Save iptables
 iptables-save > /etc/iptables.up.rules
 
 # Disable ipv6
-echo "#disable ipv6" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "Ignore ICMP request:" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf
-echo "Ignore Broadcast request:" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
+CheckEntry("#disable ipv6","/etc/sysctl.conf")
+
+#echo "#disable ipv6" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "Ignore ICMP request:" >> /etc/sysctl.conf
+#echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf
+#echo "Ignore Broadcast request:" >> /etc/sysctl.conf
+#echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
 
 sysctl -p
