d6879530e31b4426f16810f5d8064ddb1826437f
dtctd <udtctd@gmail.com>
2015-12-07 15:23:20 +0100

    .

--- harden.sh
+++ harden.sh
@@ -14,19 +14,14 @@ SYSCTL_CONF_FILE="/etc/sysctl.conf"
 DOWNLOAD_URL="https://github.com/dtctd/debian/raw/master/"
 LOG_FILE=$HOME_DIRECTORY"Hardening.log"
 
-DIALOG_WIDTH=70
-SCRIPT_TITLE="Debian hardening script v$SCRIPT_VERSION for Debian 8.2 by Sander de Wit"
-
 function showInfo() {
     CUR_DATE=$(date +%Y-%m-%d"  "%H:%M)
     echo "$CUR_DATE - INFO :: $@" >> $LOG_FILE
-    #dialog --title "Installing & Configuring..." --backtitle "$SCRIPT_TITLE" --infobox "\n$@" 5 $DIALOG_WIDTH
 }
 
 function showError() {
     CUR_DATE=$(date +%Y-%m-%d"  "%H:%M)
     echo "$CUR_DATE - ERROR :: $@" >> $LOG_FILE
-    #dialog --title "Error" --backtitle "$SCRIPT_TITLE" --msgbox "$@" 8 $DIALOG_WIDTH
 }
 
 function download() {
@@ -37,15 +32,9 @@ function download() {
 function move() {
     SOURCE="$1"
     DESTINATION="$2"
-    IS_ROOT="$3"
 
     if [ -e "$SOURCE" ]; then
-        if [ "$IS_ROOT" == "0" ]; then
-            mv "$SOURCE" "$DESTINATION" > /dev/null 2>&1
-        else
-            #sudo mv "$SOURCE" "$DESTINATION" > /dev/null 2>&1
-            mv "$SOURCE" "$DESTINATION" > /dev/null 2>&1
-        fi
+        mv "$SOURCE" "$DESTINATION" > /dev/null 2>&1
 
         if [ "$?" == "0" ]; then
             echo 1
@@ -59,36 +48,12 @@ function move() {
     fi
 }
 
-function createFile() {
-    FILE="$1"
-    IS_ROOT="$2"
-    REMOVE_IF_EXISTS="$3"
-
-    if [ -e "$FILE" ] && [ "$REMOVE_IF_EXISTS" == "1" ]; then
-        #sudo rm "$FILE" > /dev/null
-        rm "$FILE" > /dev/null
-    else
-        if [ "$IS_ROOT" == "0" ]; then
-            touch "$FILE" > /dev/null
-        else
-            #sudo touch "$FILE" > /dev/null
-            touch "$FILE" > /dev/null
-        fi
-    fi
-}
-
 function createDirectory() {
     DIRECTORY="$1"
     GOTO_DIRECTORY="$2"
-    IS_ROOT="$3"
 
     if [ ! -d "$DIRECTORY" ]; then
-        if [ "$IS_ROOT" == "0" ]; then
-            mkdir -p "$DIRECTORY" > /dev/null 2>&1
-        else
-            #sudo mkdir -p "$DIRECTORY" > /dev/null 2>&1
-            mkdir -p "$DIRECTORY" > /dev/null 2>&1
-        fi
+        mkdir -p "$DIRECTORY" > /dev/null 2>&1
     fi
 
     if [ "$GOTO_DIRECTORY" == "1" ]; then
@@ -99,30 +64,16 @@ function createDirectory() {
 function handleFileBackup() {
     FILE="$1"
     BACKUP="$1.bak"
-    IS_ROOT="$2"
-    DELETE_ORIGINAL="$3"
+    DELETE_ORIGINAL="$2"
 
     if [ -e "$BACKUP" ]; then
-        if [ "$IS_ROOT" == "1" ]; then
-            #sudo rm "$FILE" > /dev/null 2>&1
-            rm "$FILE" > /dev/null 2>&1
-            #sudo cp "$BACKUP" "$FILE" > /dev/null 2>&1
-            cp "$BACKUP" "$FILE" > /dev/null 2>&1
-        else
-            rm "$FILE" > /dev/null 2>&1
-            cp "$BACKUP" "$FILE" > /dev/null 2>&1
-        fi
+        rm "$FILE" > /dev/null 2>&1
+        cp "$BACKUP" "$FILE" > /dev/null 2>&1
     else
-        if [ "$IS_ROOT" == "1" ]; then
-            #sudo cp "$FILE" "$BACKUP" > /dev/null 2>&1
-            cp "$FILE" "$BACKUP" > /dev/null 2>&1
-        else
-            cp "$FILE" "$BACKUP" > /dev/null 2>&1
-        fi
+        cp "$FILE" "$BACKUP" > /dev/null 2>&1
     fi
 
     if [ "$DELETE_ORIGINAL" == "1" ]; then
-        #sudo rm "$FILE" > /dev/null 2>&1
         rm "$FILE" > /dev/null 2>&1
     fi
 }
@@ -130,7 +81,7 @@ function handleFileBackup() {
 function installSysctlConfig() {
     showInfo "Optimizing and hardening Debian..."
     handleFileBackup "$SYSCTL_CONF_FILE" 1 1
-    createDirectory "$TEMP_DIRECTORY" 1 0
+    createDirectory "$TEMP_DIRECTORY" 1
         download $DOWNLOAD_URL"sysctl.conf"
 
         if [ -e $TEMP_DIRECTORY"sysctl.conf" ]; then
@@ -148,7 +99,7 @@ function installSysctlConfig() {
 
 function configureIptables() {
     showInfo "Configuring iptables rules..."
-    createDirectory "$TEMP_DIRECTORY" 1 0
+    createDirectory "$TEMP_DIRECTORY" 1
         download $DOWNLOAD_URL"iptables.rules"
 
         if [ -e $TEMP_DIRECTORY"iptables.rules" ]; then
@@ -166,14 +117,13 @@ function configureIptables() {
 
 function configureIptablesLoad() {
     showInfo "Configuring pre-up load script for iptables..."
-    createDirectory "$TEMP_DIRECTORY" 1 0
+    createDirectory "$TEMP_DIRECTORY" 1
         download $DOWNLOAD_URL"iptablesload"
 
         if [ -e $TEMP_DIRECTORY"iptablesload" ]; then
             IS_MOVED=$(move $TEMP_DIRECTORY"iptablesload" "/etc/network/if-pre-up.d/iptablesload")
 
             if [ "$IS_MOVED" == "1" ]; then
-                #sudo chmod +x /etc/network/if-pre-up.d/iptablesload
                 chmod +x /etc/network/if-pre-up.d/iptablesload
                 showInfo "Iptablesload setup succeeded"
             else
@@ -186,14 +136,13 @@ function configureIptablesLoad() {
 
 function configureIptablesSave() {
     showInfo "Configuring pre-up script for iptables..."
-    createDirectory "$TEMP_DIRECTORY" 1 0
+    createDirectory "$TEMP_DIRECTORY" 1
         download $DOWNLOAD_URL"iptablessave"
 
         if [ -e $TEMP_DIRECTORY"iptablessave" ]; then
             IS_MOVED=$(move $TEMP_DIRECTORY"iptablessave" "/etc/network/if-pre-up.d/iptablessave")
 
             if [ "$IS_MOVED" == "1" ]; then
-                #sudo chmod +x /etc/network/if-pre-up.d/iptablessave
                 chmod +x /etc/network/if-pre-up.d/iptablessave
                 showInfo "Iptablessave setup succeeded"
             else
@@ -206,37 +155,28 @@ function configureIptablesSave() {
 
 function cleanUp() {
     showInfo "Cleaning up..."
-    #sudo apt-get -y autoremove > /dev/null 2>&1
     apt-get -y autoremove > /dev/null 2>&1
     sleep 1
-    #sudo apt-get -y autoclean > /dev/null 2>&1
     apt-get -y autoclean > /dev/null 2>&1
     sleep 1
-    #sudo apt-get -y clean > /dev/null 2>&1
     apt-get -y clean > /dev/null 2>&1
     sleep 1
 
     if [ -e "$TEMP_DIRECTORY" ]; then
-        #sudo rm -R "$TEMP_DIRECTORY" > /dev/null 2>&1
         rm -R "$TEMP_DIRECTORY" > /dev/null 2>&1
     fi
 }
 
-# Configure iptable rules
-#iptables -F INPUT
-#iptables -F OUTPUT
-#iptables -F FORWARD
-#iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A INPUT -s 127.0.0.1/32 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
-#iptables -A INPUT -j DROP
-#iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -j DROP
-#iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A OUTPUT -m state --state NEW -j ACCEPT
-
-installSysctlConfig
-configureIptables
-configureIptablesLoad
-configureIptablesSave
-cleanUp
+# Check if the script runs as root
+if [ $EUID -ne 0 ]; then
+    clear
+    echo "This script myst be run as root" 1>&2
+    echo ""
+    exit 1
+else
+    installSysctlConfig
+    configureIptables
+    configureIptablesLoad
+    configureIptablesSave
+    cleanUp
+fi
